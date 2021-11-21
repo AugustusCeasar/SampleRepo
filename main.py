8270c151e76d88750fb86254080bdb3eaa9d662d
AugustusCeasar <agustinplazareino@gmail.com>
2021-11-22 00:21:45 +0100

    First Commit of the Shiny Branch

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     test = [5, 4, 3, 2, 1, 0]
-    for i in test:
-        print(test)
+    evens_test = [x for x in test if x%2==0]
+    for i in evens_test:
+        print(i)
 
 
 if __name__ == '__main__':
