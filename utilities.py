3f8521944229f4584c2e722cb510fe8591e90c16
AugustusCeasar <agustinplazareino@gmail.com>
2021-12-06 00:14:21 +0100

    added the untested feature to reverse the list before printing it.

--- utilities.py
+++ utilities.py
@@ -1,6 +1,6 @@
-def print_all_the_things(list, **kwargs):
-    for x in list:
+def print_all_the_things(list, debug=false, **kwargs):
+    for x in list if ("reverse" not in kwargs or not kwargs["reverse]) else reverse(list):
         print(x)
-    if kwargs:
+    if debug and kwargs:
         for k,v in **kwargs.items():
             print(f"{k}:{v}")
