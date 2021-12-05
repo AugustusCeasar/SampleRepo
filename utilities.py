ab0460e9cf9e74eeb3541f0f0e70e4a2e8bceaa9
AugustusCeasar <agustinplazareino@gmail.com>
2021-12-06 00:21:15 +0100

    being responsible in branch master, adding testing and fixing bugs

--- utilities.py
+++ utilities.py
@@ -2,5 +2,13 @@ def print_all_the_things(list, **kwargs):
     for x in list:
         print(x)
     if kwargs:
-        for k,v in **kwargs.items():
+        for k,v in kwargs.items():
             print(f"{k}:{v}")
+
+def test_this_file():
+    test_l = [1,2,3,4,5,6,7]
+    print_all_the_things([1,2,3,4,5,6,7])
+    print_all_the_things([1,2,3,4,5,6,7], randomarg="testin")
+
+if __name__ == '__main__':
+    test_this_file()
