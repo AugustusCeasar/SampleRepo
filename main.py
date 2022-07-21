2d8fc704d8479269759befd9ab06438962b1ea99
AugustusCeasar <agustinplazareino@gmail.com>
2022-07-21 02:06:15 +0200

    added second test

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def main():
-    test = range(200)
-    evens_test = [x for x in test if x%2==0]
+    test = range(100)
+    for i in test:
+        print(test)
+    
+    test2 = range(200)
+    evens_test = [x for x in test2 if x%2==0]
     for i in evens_test:
-        print(i + 3)
+        print(i / 2)
 
 
 if __name__ == '__main__':
